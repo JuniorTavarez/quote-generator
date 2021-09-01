6e5885479ea24ec69cc06483153acb4efd870da9
juniortavarez <jtavarez8@gmail.com>
2021-09-01 12:10:37 -0400

    updated almost done

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const newQuote = document.getElementById('new-quote');
+
+console.log(quote.innerText);
+let apiQuotes = [];
+
 // Get Quotes From API
-async function getQuotes(){
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
 
+    const randomNumber = function () {
+      return Math.floor(Math.random() * apiQuotes.length - 1) + 1;
+    };
 
+    quote.innerHTML = apiQuotes[randomNumber()].text;
+    author.innerHTML = apiQuotes[randomNumber()].author;
 
-
-    
-    const apiUrl = 'https://type.fit/api/quotes'
+    newQuote.addEventListener('click', function () {
+      quote.innerHTML = apiQuotes[randomNumber()].text;
+      author.innerHTML = apiQuotes[randomNumber()].author;
+    });
+    apiQuotes.length;
+  } catch (error) {
+    //    Carch Error Here
+  }
 }
+
+// On Load
+getQuotes();
