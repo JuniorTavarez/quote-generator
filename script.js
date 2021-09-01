dd46040b04032fd6d342ab29f59b20c98c3a0387
juniortavarez <jtavarez8@gmail.com>
2021-09-01 18:27:04 -0400

    completed

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-const quoteContainer = document.getElementById('quote-Container');
+const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const author = document.getElementById('author');
 const newQuoteBtn = document.getElementById('new-quote');
 const twitter = document.getElementById('twitter');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 const newQuote = function () {
+  loading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
   quoteText.innerText = quote.text;
-  author.innerText = quote.author ? quote.author : 'Unknown';
+  //   Check if Author field is blank and replace it with Unknown
+  author.innerText = !quote.author ? 'Unknown' : quote.author;
+  //   long text
+  quote.text.length > 120
+    ? quoteText.classList.add('long-quote')
+    : //   quoteText.classList.remove('quote-text')
+      quoteText.classList.remove('long-quote');
+  // quoteText.classList.add('quote-text')
 
-  newQuoteBtn.addEventListener('click', newQuote);
+  complete();
 };
 
 // Get Quotes From API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
@@ -27,6 +48,16 @@ async function getQuotes() {
   }
 }
 
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${author.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitter.addEventListener('click', tweetQuote);
+
 // On Load
 getQuotes();
-// newQuote();
